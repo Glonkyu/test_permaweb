442df29321cf43ff3a5a4b707bee5614bcabd20f
Glonkyu <101358874+Glonkyu@users.noreply.github.com>
2024-12-30 19:17:24 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import './index.css'; // Global CSS
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Router>
-            <App />
-        </Router>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
