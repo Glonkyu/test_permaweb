bfcbd21e35531d553589bf49446b8a01b895c312
Glonkyu <101358874+Glonkyu@users.noreply.github.com>
2024-12-30 15:09:02 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+const facts = [
+    "Cats sleep for 70% of their lives.",
+    "A group of cats is called a clowder.",
+    "Cats have over 20 muscles in their ears.",
+    "Domestic cats can run up to 30 mph.",
+    "A cat's whiskers are roughly as wide as its body."
+];
